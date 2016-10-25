fefa11490c1a100aa7e9114d2866da0ada5dae4c
Navyata <navyata@navy-ubuntu.(none)>
2016-10-25 17:15:47 -0400

    Faster implementation

--- particle_debug.py
+++ particle_debug.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import numpy as np
+from multiprocessing import Pool
 from scipy.spatial.distance import cdist
 from scipy.stats import norm
 import numpy.random
@@ -15,14 +16,14 @@ class particle:
 		self.map = np.loadtxt('wean.dat', delimiter=' ')
 		self.occ = np.loadtxt('occu.dat', delimiter=' ')
 		self.unocc = np.loadtxt('unoccu.dat', delimiter=' ')
-		self.unocc_list = np.loadtxt('unoccu.dat', delimiter=' ').tolist()
+		self.unocc_dict = {tuple(el):1 for el in self.unocc}
 		self.unocc_corr = np.loadtxt('unoccu_corr.dat', delimiter=' ')
 		#self.X_init = np.loadtxt('part_init.dat', delimiter=' ')
-		self.num_p = 2*1e3
+		self.num_p = 1e4
 		self.sense = np.loadtxt('sense.dat', delimiter=' ')
 		self.isodom = np.loadtxt('is_odom.dat', delimiter=' ')
 		self.mindist = np.loadtxt('min_d.dat', delimiter=' ')
-		self.a = np.array([0.001,0.0001,0.1,0.1])
+		self.a = np.array([1e-6,1e-6,0.1,0.1])
 		self.c_lim = 10
 		self.lsr_max = 1000
 		self.zmax = 0.25
@@ -46,6 +47,9 @@ class particle:
 		sig_t = np.finfo(float).eps + np.sqrt(self.a[2] * np.absolute(t) + self.a[3] * np.absolute(r1+r2))
 		sig_r1 = np.finfo(float).eps + np.sqrt(self.a[0] * np.absolute(r1) + self.a[1] * np.absolute(t))
 		sig_r2 = np.finfo(float).eps + np.sqrt(self.a[0] * np.absolute(r2) + self.a[1] * np.absolute(t))
+		print('heeee')		
+		print(sig_r1)
+		print(sig_r2)		
 		h_t = np.reshape(t + np.random.normal(0,sig_t,len(X_t)), (len(X_t),1))
 		h_r1 = r1 + np.random.normal(0,sig_r1,len(X_t))
 		h_r2 = r2 + np.random.normal(0,sig_r2,len(X_t))
@@ -54,10 +58,10 @@ class particle:
 		ang = np.reshape(X_t[:,2]+h_r1+h_r2, (len(X_t),1))
 		ang = (ang + (- 2*np.pi)*(ang > np.pi) + (2*np.pi)*(ang < -np.pi))
 		X_upd = np.concatenate((pos,ang), axis=1)
-		map_c = np.ceil(pos/10).astype(int).tolist()
+		map_c = np.ceil(pos/10).astype(int)
 		count = 0;
 		for i in range(len(X_upd)):
-			if(map_c[i] in self.unocc_list):
+			if(self.unocc_dict.has_key(tuple(map_c[i]))):
 				X_upd[count] = X_upd[i]
 				count = count+1
 		print(count)
@@ -70,9 +74,7 @@ class particle:
 		return (X_upd[:,:2] + np.concatenate((np.cos(th), np.sin(th)), axis=1) * t)
 
 
-	def get_wt(self, x_upd, lsr_pos, L):
-		angs = np.arange(-np.pi/2, np.pi/2 + np.pi/180, np.pi/180)
-		inds = np.arange(self.srt-1,self.end,self.step)
+	def get_wt(self, x_upd, lsr_pos, L, angs, inds):
 		th = np.reshape(x_upd[2] + angs[inds], (len(inds),1))
 		t = np.reshape(L[6+inds], (len(inds),1))
 		meas_pos = lsr_pos + np.concatenate((np.cos(th), np.sin(th)), axis=1) * t
@@ -83,18 +85,32 @@ class particle:
 			min_c = np.floor(meas_pos[i]/10).astype(int)
 			if(min_c[0] >= len(self.mindist) or min_c[1] >= len(self.mindist[0]) 
 				or min_c[0] < 0 or min_c[1] > 0):
+				q = q*self.zrand/self.lsr_max
 				continue
 			d = self.mindist[min_c[0], min_c[1]]
 			q = q*(self.zhit*norm.pdf(d, 0, self.sig_h) + self.zrand/self.lsr_max)
 		return q
 
 
-	def get_wt_vect(self, X_upd, i):
+	def get_wt_vect(self, X_upd, i, angs, inds):
 		L = self.sense[i]
 		lsr_poses = self.get_lsr_poses(X_upd, L)
 		wt_vect = np.empty([len(X_upd),])
-		for i in range(len(wt_vect)):
-			wt_vect[i] = self.get_wt(X_upd[i], lsr_poses[i], L)
+		wt_vect.fill(self.q)
+		for i in inds.tolist():
+			th = np.reshape(X_upd[:,2] + angs[i], (len(X_upd),1))
+			t = L[6+i]
+			meas_pos = lsr_poses + np.concatenate((np.cos(th), np.sin(th)), axis=1) * t
+			if(t > self.lsr_max):
+				continue
+			min_c = np.floor(meas_pos/10).astype(int)
+			for j in range(len(meas_pos)):
+				if(min_c[j,0] >= len(self.mindist) or min_c[j,1] >= len(self.mindist[0]) 
+					or min_c[j,0] < 0 or min_c[j,1] > 0):
+					wt_vect[j] = wt_vect[j]*self.zrand/self.lsr_max
+					continue
+				d = self.mindist[min_c[j,0], min_c[j,1]]
+				wt_vect[j] = wt_vect[j]*(self.zhit*norm.pdf(d, 0, self.sig_h) + self.zrand/self.lsr_max)
 		return wt_vect
 
 
@@ -128,8 +144,10 @@ class particle:
 
 	def main(self):
 		X_init = self.initialize(self.num_p)
+		angs = np.arange(-np.pi/2, np.pi/2 + np.pi/180, np.pi/180)
+		inds = np.arange(self.srt-1,self.end,self.step)
 		if(not self.isodom[0]):
-			wt_vect = self.get_wt_vect(X_init, 0)
+			wt_vect = self.get_wt_vect(X_init, 0, angs, inds)
 			X_t = self.get_p_upd(wt_vect, X_init)
 		print('hi')
 		t1 = t2 = t3 = t4 = t5 = 0
@@ -139,9 +157,10 @@ class particle:
 			t1 = time.time()
 			X_upd = self.motion_update(X_t, O1, O2)
 			t2 = time.time()
+			t3 = t4 = t5 = 0
 			if(not self.isodom[i]):
 				t3 = time.time()
-				wt_vect = self.get_wt_vect(X_upd,i)
+				wt_vect = self.get_wt_vect(X_upd, i, angs, inds)
 				t4 = time.time()
 				X_upd = self.get_p_upd(wt_vect, X_upd)
 				t5 = time.time()
